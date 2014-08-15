590597b304432ef0b6202230d250c27177bf2ecf
cartmanguo <nggch1987@163.com>
2014-08-15 14:04:30 +0800

    Initial Commit

---
+++ AlbumsCreate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlbumsCreate
+//
+//  Created by Line_Hu on 14-8-15.
+//  Copyright (c) 2014年 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlbumsCreate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlbumsCreate
+//
+//  Created by Line_Hu on 14-8-15.
+//  Copyright (c) 2014年 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
